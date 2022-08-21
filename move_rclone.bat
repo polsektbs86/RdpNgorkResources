5e5385961edf90f18a469ba92435df4683af2999
polsektbs86 <polsektbs86@gmail.com>
2022-08-21 16:47:17 +0700

    first commit

---
+++ move_rclone.bat
@@ -0,0 +1,6 @@
+cd %APPDATA%
+mkdir rclone
+cd rclone
+move C:\Users\Public\Desktop\rclone.conf
+ls
+PAUSE
---
+++ task_all.bat
@@ -0,0 +1,9 @@
+TITLE SYNC DRAKOR URL 1 OF 6 TO DRIVE 1
+cd C:\RcloneResources
+rclone sync drakor_url1: drive_me1:MOVIE_DRAKOR_WHATBOX/URL_1 --error-on-no-transfer --transfers 1 --progress
+rclone sync drakor_url2: drive_me1:MOVIE_DRAKOR_WHATBOX/URL_2 --error-on-no-transfer --transfers 1 --progress
+rclone sync drakor_url3: drive_me1:MOVIE_DRAKOR_WHATBOX/URL_3 --error-on-no-transfer --transfers 1 --progress
+rclone sync drakor_url4: drive_me1:MOVIE_DRAKOR_WHATBOX/URL_4 --error-on-no-transfer --transfers 1 --progress
+rclone sync drakor_url5: drive_me1:MOVIE_DRAKOR_WHATBOX/URL_5 --error-on-no-transfer --transfers 1 --progress
+rclone sync drakor_url6: drive_me1:MOVIE_DRAKOR_WHATBOX/URL_6 --error-on-no-transfer --transfers 1 --progress
+PAUSE
